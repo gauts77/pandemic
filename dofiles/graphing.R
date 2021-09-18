dd34f391e9be3eee75b347ca059812bc2b413320
gauts77 <gautamvyas123@gmail.com>
2021-09-18 12:46:26 +0100

    graphs
    updated graphs + added facet wrap graph

--- dofiles/graphing.R
+++ dofiles/graphing.R
@@ -15,17 +15,36 @@ indx_list <- unique(stock$Index)
 for(indx in indx_list){
   
   stock_to_plot <- stock %>%
-    filter(Index == indx) ##issue with data here - just coming up with one value. Haven't assigned stock returns as the data.
+    filter(Index == indx) 
   
   my_plot <- ggplot(data = stock_to_plot, aes(x = Week, y = Closing.Price)) +
-    geom_point() +
-    geom_line(aes(group = 1)) +
-    geom_vline(xintercept = as.numeric(stock$Week[9]), linetype = 4, colour = "red")+
-    labs(title = paste(indx, "Time Series", sep = " "), x = "Month", y = "Closing Price") ## will be good to add a horizontal line at 12/03/21. Also just make the graphs look better in general.
+    geom_point(colour = "blue", size = 0.5) +
+    geom_line(aes(group = 1), colour = "blue") +
+    theme_minimal() +
+    geom_vline(xintercept = as.numeric(stock$Week[9]), linetype = 4, colour = "red") +
+    geom_vline(xintercept = as.numeric(stock$Week[10]), linetype = 4, colour = "red") +
+    labs(title = paste(indx, "Time Series", sep = " "), x = "Month", y = "Closing Price") +
+    geom_rect(xmin = as.numeric(stock$Week[9]), xmax = as.numeric(stock$Week[10]), ymin = 0, ymax = Inf, fill = "red", alpha = 0.01)
   
   ggsave(filename = paste(indx, "_time_series.png", sep = ""), plot = my_plot, path = "C:/Users/gauta/Documents/GitHub/pandemic/figures/stock_time_series")
   
 }
+
+##facet graph:
+p <- ggplot(data = stock, aes(x = Week, y = Closing.Price)) +
+  geom_point(size = 0.001) +
+  facet_wrap(~ï..Country, nrow = 5, scale = "free_y") + 
+  geom_line(aes(group = 1), colour = "blue") +
+  theme_minimal() +
+  labs(title = paste(indx, "Time Series", sep = " "), x = "Month", y = "Closing Price") +
+  theme(axis.text.y = element_blank()) +
+  geom_vline(xintercept = as.numeric(stock$Week[9]), linetype = 4, colour = "red") +
+  geom_vline(xintercept = as.numeric(stock$Week[10]), linetype = 4, colour = "red") +
+  geom_rect(xmin = as.numeric(stock$Week[9]), xmax = as.numeric(stock$Week[10]), ymin = 0, ymax = Inf, fill = "red", alpha = 0.01)
+
+ggsave(filename = "grouped_time_series.png", plot = p, path = "C:/Users/gauta/Documents/GitHub/pandemic/figures/stock_time_series")
+  
+  
 ##todo:
   ## adjust y-axis
   ## add a single graph with them all using facet wrap.
