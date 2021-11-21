aadc2a1d3d8485e90bd7006aecbd5d0e1fb89802
gauts77 <gautamvyas123@gmail.com>
2021-11-21 18:23:47 +0000

    21 11

--- dofiles/graphing.R
+++ dofiles/graphing.R
@@ -110,4 +110,115 @@ q <- ggplot(data = stock, aes(x = week, y = rel_closingprice, group = Ã.country)
   geom_rect(xmin = as.numeric(stock$week[9]), xmax = as.numeric(stock$week[11]), ymin = 0, ymax = Inf, fill = "red", alpha = 0.005)
 
 ggsave(filename = "rel_grouped_time_series_2.png", plot = q, path = "C:/Users/gauta/Documents/GitHub/pandemic/figures/stock_time_series/relative")
+
+
+#Plotting Time Series Graphs of Hospital beds
+
+rm(list = ls())
+setwd('C:/Users/gauta/Documents/GitHub/pandemic/Data/Xvar')
+
+
+library(tidyverse)
+library(readr)
+library(ggplot2)
+library(countrycode)
+stock <- read_csv('WB_Hosp_Beds_p1000.csv')
+
+stock$continent <- countrycode(sourcevar = stock[["Country"]],
+                               origin = "country.name",
+                               destination = "continent")
+
+cntry_list <- unique(stock$Country)
+
+for(cntry in cntry_list){
   
+  stock_to_plot <- stock %>%
+    filter(Country == cntry) 
+  
+  my_plot <- ggplot(data = stock_to_plot, aes(x = Year, y = HB_p1000)) +
+    geom_point(colour = "dodgerblue", size = 0.5) +
+    geom_line(aes(group = 1), colour = "blue") +
+    theme_minimal() + 
+    labs(title = paste(cntry, "Hospital Beds/1000 Over Time", sep = " "), x = "Year", y = "Hospital Beds/1000")
+    
+  
+  ggsave(filename = paste(cntry, "_time_series.png", sep = ""), plot = my_plot, path = "C:/Users/gauta/Documents/GitHub/pandemic/figures/xvars/hbeds/all_countries")
+  
+}
+
+
+#European Subset of Countries: Hospital Beds
+rm(list = ls())
+setwd('C:/Users/gauta/Documents/GitHub/pandemic/Data/Xvar')
+
+
+library(tidyverse)
+library(readr)
+library(ggplot2)
+library(countrycode)
+stock <- read_csv('WB_Hosp_Beds_p1000.csv')
+
+stock$continent <- countrycode(sourcevar = stock[["Country"]],
+                               origin = "country.name",
+                               destination = "continent")
+
+df2 <- subset(stock, continent != "Asia" & continent != "NA" & continent != "Africa" & continent != "Americas" & continent != "Oceania")
+
+cntry_list <- unique(df2$Country)
+
+for(cntry in cntry_list){
+  
+  stock_to_plot <- df2 %>%
+    filter(Country == cntry) 
+  
+  my_plot <- ggplot(data = stock_to_plot, aes(x = Year, y = HB_p1000)) +
+    geom_point(colour = "dodgerblue", size = 0.5) +
+    geom_line(aes(group = 1), colour = "blue") +
+    theme_minimal() + 
+    labs(title = paste(cntry, "Hospital Beds/1000 Over Time", sep = " "), x = "Year", y = "Hospital Beds/1000")
+  
+  
+  ggsave(filename = paste(cntry, "_time_series_EUR.png", sep = ""), plot = my_plot, path = "C:/Users/gauta/Documents/GitHub/pandemic/figures/xvars/hbeds/europe")
+
+}
+
+#European Facet
+
+rm(list = ls())
+setwd('C:/Users/gauta/Documents/GitHub/pandemic/Data/Xvar')
+
+
+library(tidyverse)
+library(readr)
+library(ggplot2)
+library(countrycode)
+stock <- read_csv('WB_Hosp_Beds_p1000.csv')
+
+stock$continent <- countrycode(sourcevar = stock[["Country"]],
+                               origin = "country.name",
+                               destination = "continent")
+
+df2 <- subset(stock, continent != "Asia" & continent != "NA" & continent != "Africa" & continent != "Americas" & continent != "Oceania")
+
+cntry_list <- unique(df2$Country)
+p <- ggplot(data = df2, aes(x = Year, y = HB_p1000)) +
+  geom_point(colour = "dodgerblue", size = 0.5) +
+  geom_line(aes(group = 1), colour = "blue") +
+  facet_wrap(~Country, nrow = 10, scale = "free_y") +
+  theme_minimal() + 
+  theme(axis.text.y = element_blank()) +
+  theme(axis.text.x = element_text(angle = 90)) +
+  labs(title = paste("Hospital Beds/1000 Over Time", sep = " "), x = "Year", y = "Hospital Beds/1000")
+
+ggsave(filename = paste("facet_time_series_EUR.png", sep = ""), plot = p,  path = "C:/Users/gauta/Documents/GitHub/pandemic/figures/xvars/hbeds/europe")
+
+
+
+
+
+
+
+
+
+
+
--- dofiles/sidecode.do
+++ dofiles/sidecode.do
@@ -36,14 +36,26 @@ drop if y == 1
 drop y
 
 merge 1:1 Country using "C:/Users/gauta/Documents/GitHub/pandemic/Data/Xvar/master_X"
+drop _merge
+
 //next: drop duplicates by getting rid of all vars except country and max cases, then finding out how to drop duplicates. Then merge with World Bank data (both Xvars), and draw some plots/run regression. 
 //then download economic growth data, and add to the regression, then generate hospital bed growth rate after h1n1 as a independent var.
 save h1n1_1.dta, replace
 
+*adding GDP/capita
+use "C:\Users\gauta\Documents\Diss_Section_3.2\pwt100.dta", clear
+keep country year rgdpna pop
+rename country Country
+drop if year != 2018
 
-*adding health expenditure data to the above dataset.
+gen gdp_cap = rgdpna/pop
 
-keep Country id max_cases _merge WB_hb_p1000 WHO_2018
+merge 1:1 Country using h1n1_1.dta
+drop _merge
+save h1n1_1.dta, replace
+
+
+keep Country id max_cases _merge WB_hb_p1000 WHO_2018 rgdpna gdp_cap
 
 
 *graphing - dropping a value here, so reinstate it after!
@@ -56,17 +68,41 @@ scatter WHO_2018 max_cases, title({bf:H1N1 Cases and Health Expenditure}, size(8
 
 
 use h1n1_1.dta, clear
-asdoc regress WB_hb_p1000 max_cases, robust replace dec(5)
-asdoc regress WHO_2018 max_cases, robust replace dec(5)
+asdoc regress WB_hb_p1000 max_cases gdp_cap, robust replace dec(5)
+asdoc regress WHO_2018 max_cases gdp_cap, robust replace dec(5)
 
-*gen the other thing - rate of increase. regress and graph. then do the first thing (aggregate by continent.) Then make a doc.
 
 * Rate of Increase
-import excel "C:/Users/gauta/Documents/GitHub/pandemic/Data/Xvar/WB_Hosp_Beds_p1000.xlsx", sheet("Table2") firstrow clear
+/*import excel "C:/Users/gauta/Documents/GitHub/pandemic/Data/Xvar/WB_Hosp_Beds_p1000.xlsx", sheet("Table2") firstrow clear
 encode CountryName, gen(id)
 rename Attribute Year
 encode Year, gen(Yearid)
 rename Value WB_hb_p1000
 xtset id Yearid
 
-drop if Year != inlist("2010" "2011" "2012" "2013" "2014" "2015")
+gen ok = 1 if Year == "2010"
+replace ok = 1 if Year == "2011"
+replace ok = 1 if Year == "2012"
+
+drop if ok != 1
+drop ok*/
+
+
+use "C:/Users/gauta/Documents/GitHub/pandemic/Data/Other/rchange.dta", clear
+gen baseyear = 1 if Year == "2012"
+foreach i in id {
+	gen rate1012 = ((WB_hb_p1000-WB_hb_p1000[_n-2])/(WB_hb_p1000[_n-2])) if baseyear == 1
+}
+
+drop if baseyear != 1
+drop baseyear
+
+rename CountryName Country
+merge 1:1 Country using "C:/Users/gauta/Documents/GitHub/pandemic/Data/Other/h1n1_1.dta"
+
+keep Country id max_cases _merge WB_hb_p1000 WHO_2018  gdp_cap rate1012
+
+asdoc regress rate1012 max_cases gdp_cap, robust replace dec(5)
+
+
+
